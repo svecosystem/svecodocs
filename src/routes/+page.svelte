cbba065b087929f2490001534112ec6fd36d02ec
Hunter Johnston <johnstonhuntera@gmail.com>
2024-01-19 15:57:22 -0500

    homepage placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="relative mx-auto max-w-5xl pt-20 sm:pt-24 lg:pt-32">
+	<h1
+		class="text-balance text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl"
+	>
+		Docs starter template for <span class="text-orange-500">SvelteKit</span>
+	</h1>
+</div>
