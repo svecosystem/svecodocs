2f76da050b7ccce08c5720619a4347f3deb73a8f
Hunter Johnston <johnstonhuntera@gmail.com>
2024-01-19 17:45:10 -0500

    update docs

--- src/lib/components/layout/site-header/header-logo.svelte
+++ src/lib/components/layout/site-header/header-logo.svelte
@@ -2,8 +2,13 @@
 	import { SvecosystemDark, SvecosystemLight } from "$lib/components/logos";
 </script>
 
-<a href="/" class="mr-3 w-[2rem] flex-none overflow-hidden md:w-auto">
-	<SvecosystemLight class="block h-5 w-auto dark:hidden" />
-	<SvecosystemDark class="hidden h-5 w-auto dark:block" />
-	<span class="sr-only"> Current Page Here </span>
-</a>
+<div class="flex items-center gap-1.5 lg:flex-1">
+	<a
+		href="/"
+		class="flex flex-shrink-0 items-end gap-1.5 text-xl font-bold text-gray-900 dark:text-white"
+	>
+		<SvecosystemLight class="block h-5 w-auto dark:hidden" />
+		<SvecosystemDark class="hidden h-5 w-auto dark:block" />
+		<span class="sr-only"> Current Page Here </span>
+	</a>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,7 @@ const config = {
 				ring: "hsl(var(--ring) / <alpha-value>)",
 				background: "hsl(var(--background) / <alpha-value>)",
 				foreground: "hsl(var(--foreground) / <alpha-value>)",
+				brand: "hsl(var(--brand) / <alpha-value>)",
 				primary: {
 					DEFAULT: "hsl(var(--primary) / <alpha-value>)",
 					foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
