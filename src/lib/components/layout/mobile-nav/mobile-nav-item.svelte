137cddbe285e65c7faa2ed37912ec8048fffbf92
Hunter Johnston <johnstonhuntera@gmail.com>
2024-01-19 21:18:29 -0500

    cleanup mobile nav link

--- src/lib/components/layout/mobile-nav/mobile-nav-item.svelte
+++ src/lib/components/layout/mobile-nav/mobile-nav-item.svelte
@@ -2,7 +2,7 @@
 	import { page } from "$app/stores";
 	import type { SidebarNavItem } from "$lib/config";
 	import { isTitleActive, slugFromPathname } from "$lib/utils/docs";
-	import { Drawer } from "vaul-svelte";
+	import MobileNavLink from "./mobile-nav-link.svelte";
 
 	export let navItem: SidebarNavItem;
 	$: currentPath = slugFromPathname($page.url.pathname);
@@ -14,27 +14,21 @@
 		{#each navItem.items as item}
 			{@const isActive = isTitleActive(currentPath, item.title)}
 			<li>
-				<Drawer.Close asChild let:builder>
-					<a
-						use:builder.action
-						href={item.href}
-						class="-ml-px pl-4 {isActive
-							? 'border-brand text-brand'
-							: 'border-border text-muted-foreground hover:border-primary hover:text-primary'}"
-						>{item.title}</a
-					>
-				</Drawer.Close>
+				<MobileNavLink
+					class="-ml-px pl-4 {isActive
+						? 'border-brand text-brand'
+						: 'border-border text-muted-foreground'}
+					"
+					href={item.href}
+				></MobileNavLink>
 			</li>
 		{/each}
 	</ul>
 {:else}
-	<Drawer.Close asChild let:builder>
-		<a
-			href={navItem.href}
-			use:builder.action
-			class="group mb-4 flex items-center font-semibold lg:text-sm lg:leading-6"
-		>
-			{navItem.title}
-		</a>
-	</Drawer.Close>
+	<MobileNavLink
+		href={navItem.href}
+		class="group mb-4 flex items-center font-semibold lg:text-sm lg:leading-6"
+	>
+		{navItem.title}
+	</MobileNavLink>
 {/if}
---
+++ src/lib/components/layout/mobile-nav/mobile-nav-link.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { HTMLAnchorAttributes } from "svelte/elements";
+	import { Drawer } from "vaul-svelte";
+
+	type $$Props = HTMLAnchorAttributes;
+
+	export let href: $$Props["href"] = "";
+</script>
+
+<Drawer.Close asChild let:builder>
+	<a use:builder.action {href} {...$$restProps}>
+		<slot />
+	</a>
+</Drawer.Close>
